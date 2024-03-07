0a8b5275adfa37aa988a385c6ccd9dc12c8ebef2
InDanity <donyemahn@gmail.com>
2024-03-06 19:23:46 -0800

    Set up console app and put basic reader code to make sure it can communicate with db.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string constr = "server=localhost;uid=root;pwd=JollofisNeat;database=atm";
+        MySqlConnection con = new MySqlConnection();
+        con.ConnectionString = constr;
+        con.Open();
+
+        string sql = "SELECT * FROM atm.customer";
+        MySqlCommand cmd = new MySqlCommand(sql, con);
+
+        MySqlDataReader reader = cmd.ExecuteReader();
+        while ( reader.Read() ) 
+        {
+            Console.WriteLine( reader.GetString(1) );
+        }
+
+        Console.WriteLine("Hello, world!");
+    }
+}
